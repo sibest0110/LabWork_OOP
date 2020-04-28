a86824dda1576d4eca6983110da04197df449394
Vladislav Fomenko <sibest0110@yandex.ru>
2020-04-28 11:28:43 +0700

    feature(Vehicles): добавлено [Serializable] для всех ТС

--- Фоменко ЛБ-3/Vehicles/Helicopter.cs
+++ Фоменко ЛБ-3/Vehicles/Helicopter.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
+    [Serializable]
     /// <summary>
     /// Вертолёт
     /// </summary>
--- Фоменко ЛБ-3/Vehicles/HybridCar.cs
+++ Фоменко ЛБ-3/Vehicles/HybridCar.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
+    [Serializable]
     /// <summary>
     /// Гибридный автомобиль
     /// </summary>
